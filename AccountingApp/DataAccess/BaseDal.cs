9f0515e9f1fbc65e686220b44e883be4e6dbeca0
liweihan <weihanli@outlook.com>
2017-09-26 21:02:38 +0800

    底层查询增加未删除条件

--- AccountingApp/DataAccess/BaseDal.cs
+++ AccountingApp/DataAccess/BaseDal.cs
@@ -34,7 +34,7 @@ namespace AccountingApp.DataAccess
         public async Task<bool> UpdateAsync(T entity)
         {
             entity.UpdatedTime = DateTime.Now;
-   
+
             // reslove the exception
             // cannot be tracked because another instance of this type with the same key is already being tracked
             // https://stackoverflow.com/questions/6033638/an-object-with-the-same-key-already-exists-in-the-objectstatemanager-the-object
@@ -85,11 +85,11 @@ namespace AccountingApp.DataAccess
         {
             if (isAsc)
             {
-                return await _dbEntity.Set<T>().AsNoTracking().Where(whereLamdba).OrderBy(orderbyLambda).ToListAsync();
+                return await _dbEntity.Set<T>().AsNoTracking().Where(whereLamdba.And(t => !t.IsDeleted)).OrderBy(orderbyLambda).ToListAsync();
             }
             else
             {
-                return await _dbEntity.Set<T>().AsNoTracking().Where(whereLamdba).OrderByDescending(orderbyLambda).ToListAsync();
+                return await _dbEntity.Set<T>().AsNoTracking().Where(whereLamdba.And(t => !t.IsDeleted)).OrderByDescending(orderbyLambda).ToListAsync();
             }
         }
 
@@ -98,17 +98,17 @@ namespace AccountingApp.DataAccess
             int offset = (pageIndex - 1) * pageSize;
             if (isAsc)
             {
-                return await _dbEntity.Set<T>().AsNoTracking().Where(whereLamdba).OrderBy(orderbyLambda).Skip(offset).Take(pageSize).ToListAsync();
+                return await _dbEntity.Set<T>().AsNoTracking().Where(whereLamdba.And(t => !t.IsDeleted)).OrderBy(orderbyLambda).Skip(offset).Take(pageSize).ToListAsync();
             }
             else
             {
-                return await _dbEntity.Set<T>().AsNoTracking().Where(whereLamdba).OrderByDescending(orderbyLambda).Skip(offset).Take(pageSize).ToListAsync();
+                return await _dbEntity.Set<T>().AsNoTracking().Where(whereLamdba.And(t => !t.IsDeleted)).OrderByDescending(orderbyLambda).Skip(offset).Take(pageSize).ToListAsync();
             }
         }
 
         public async Task<T> FetchAsync(int id)
         {
-            return await _dbEntity.Set<T>().AsNoTracking().FirstOrDefaultAsync(e=>e.PKID == id);
+            return await _dbEntity.Set<T>().AsNoTracking().FirstOrDefaultAsync(e => e.PKID == id);
         }
 
         public async Task<T> FetchAsync(Expression<Func<T, bool>> whereLamdba)
@@ -118,17 +118,17 @@ namespace AccountingApp.DataAccess
 
         public async Task<bool> ExistAsync(Expression<Func<T, bool>> whereLamdba)
         {
-            return await _dbEntity.Set<T>().AsNoTracking().AnyAsync(whereLamdba);
+            return await _dbEntity.Set<T>().AsNoTracking().AnyAsync(whereLamdba.And(t => !t.IsDeleted));
         }
 
         public async Task<int> QueryCountAsync(Expression<Func<T, bool>> whereLamdba)
         {
-            return await _dbEntity.Set<T>().AsNoTracking().CountAsync(whereLamdba);
+            return await _dbEntity.Set<T>().AsNoTracking().CountAsync(whereLamdba.And(t => !t.IsDeleted));
         }
 
-        public async Task<bool> DeleteAsync(Expression<Func<T, bool>> whereLamdba,string updatedBy)
+        public async Task<bool> DeleteAsync(Expression<Func<T, bool>> whereLamdba, string updatedBy)
         {
-            var list = _dbEntity.Set<T>().Where(whereLamdba).ToList();
+            var list = _dbEntity.Set<T>().Where(whereLamdba.And(t => !t.IsDeleted)).ToList();
             if (list != null && list.Any())
             {
                 for (int i = 0; i < list.Count(); i++)
